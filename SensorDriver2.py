3e870407e71f38f61f53351e68de19269e131342
Mayank Tiwari <mayank@Mayanks-MacBook-Air.local>
2023-12-04 01:52:36 -0500

    Bug Fixes

--- SensorDriver2.py
+++ SensorDriver2.py
@@ -81,8 +81,53 @@ def median_filter(data, window_size):
 
     return filtered_data
 
-value1 = sensor.Measurement(trigger_pin1, echo_pin1, temperature=68, unit="imperial")
-value2 = sensor.Measurement(trigger_pin2, echo_pin2, temperature=68, unit="imperial")
+def get_distance (trig, echo):
+
+    if GPIO.input (echo):                                               # If the 'Echo' pin is already high
+        return (100)                                                    # then exit with 100 (sensor fault)
+
+    distance = 0                                                        # Set initial distance to zero
+
+    GPIO.output (trig,False)                                            # Ensure the 'Trig' pin is low for at
+    time.sleep (0.05)                                                   # least 50mS (recommended re-sample time)
+
+    GPIO.output (trig,True)                                             # Turn on the 'Trig' pin for 10uS (ish!)
+    dummy_variable = 0                                                  # No need to use the 'time' module here,
+    dummy_variable = 0                                                  # a couple of 'dummy' statements will do fine
+    
+    GPIO.output (trig,False)                                            # Turn off the 'Trig' pin
+    time1, time2 = time.time(), time.time()                             # Set inital time values to current time
+    
+    while not GPIO.input (echo):                                        # Wait for the start of the 'Echo' pulse
+        time1 = time.time()                                             # Get the time the 'Echo' pin goes high
+        if time1 - time2 > 0.02:                                        # If the 'Echo' pin doesn't go high after 20mS
+            distance = 100                                              # then set 'distance' to 100
+            break                                                       # and break out of the loop
+        
+    if distance == 100:                                                 # If a sensor error has occurred
+        return (distance)                                               # then exit with 100 (sensor fault)
+    
+    while GPIO.input (echo):                                            # Otherwise, wait for the 'Echo' pin to go low
+        time2 = time.time()                                             # Get the time the 'Echo' pin goes low
+        if time2 - time1 > 0.02:                                        # If the 'Echo' pin doesn't go low after 20mS
+            distance = 100                                              # then ignore it and set 'distance' to 100
+            break                                                       # and break out of the loop
+        
+    if distance == 100:                                                 # If a sensor error has occurred
+        return (distance)                                               # then exit with 100 (sensor fault)
+        
+                                                                        # Sound travels at approximately 2.95uS per mm
+                                                                        # and the reflected sound has travelled twice
+                                                                        # the distance we need to measure (sound out,
+                                                                        # bounced off object, sound returned)
+                                                                        
+    distance = (time2 - time1) / 0.00000295 / 2 / 10                    # Convert the timer values into centimetres
+    return (distance)                                                   # Exit with the distance in centimetres
+    
+
+
+value1 = get_distance(trigger_pin1, echo_pin1)
+value2 = get_distance(trigger_pin2, echo_pin2)
 
 # Driver Code Starts Here
 try:
@@ -90,8 +135,8 @@ try:
     mqtt_publish_worker_thread.start()
 
     while True:
-        sensorOneDatas = [value1.distance(value1.raw_distance()) for _ in range(10)]
-        sensorTwoDatas = [value2.distance(value2.raw_distance()) for _ in range(10)]
+        sensorOneDatas = [get_distance(trigger_pin1, echo_pin1) for _ in range(10)]
+        sensorTwoDatas = [get_distance(trigger_pin2, echo_pin2) for _ in range(10)]
 
         sensorOne = median_filter(sensorOneDatas, window_size)
         sensorTwo = median_filter(sensorTwoDatas, window_size)
